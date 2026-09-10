e20be96bd44a2450691dbf511eb1000da8d2df7c
madhyumgroup <madhyumgroup@gmail.com>
2026-09-10 13:26:33 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='madhyum-crm-v1.0.0';
-const ASSETS=['./','./index.html','./app.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='madhyum-crm-v1.0.1-brand';
+const ASSETS=['./','./index.html','./app.css','./app.js','./manifest.webmanifest','./madhyum-brand.png','./icon/icon-192.png','./icon/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
