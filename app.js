50bd6372e00aaa0e90205bb86f8beb4bb145a51a
madhyumgroup <madhyumgroup@gmail.com>
2026-09-10 22:54:33 +0530

    KJ

--- app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js').catch(()=>{}));
-window.navigate=navigate;window.closeModal=closeModal;window.logout=logout;
-if(restoreSession())startApp().catch(()=>logout(true));
--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='madhyum-crm-live-v1';
-const CORE=['./','./index.html','./css/app.css','./js/app.js','./manifest.json','./madhyum-brand.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
