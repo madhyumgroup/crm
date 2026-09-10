5f2178aabe655adb5ee3645d2ba57f5c580d552d
madhyumgroup <madhyumgroup@gmail.com>
2026-09-10 20:36:00 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,11 @@
-const CACHE = 'madhyum-crm-v5';
+const CACHE = 'madhyum-crm-final-locked-v1';
 const APP = [
-  './',
-  './index.html',
-  './css/app.css',
-  './js/app.js',
-  './js/ui.js',
-  './js/database.js',
-  './js/config.js',
-  './manifest.json',
-  './icon/icon-192.png',
-  './icon/icon-512.png'
+  './','./index.html','./css/app.css','./js/app.js','./js/ui.js','./js/database.js','./js/config.js',
+  './manifest.json','./icon/icon-192.png','./icon/icon-512.png'
 ];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(APP))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(response => response || caches.match('./index.html')))
-  );
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
 });
