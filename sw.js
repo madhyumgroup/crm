4b7c6ddeae74ff793259a1d8492b158edb89e6df
madhyumgroup <madhyumgroup@gmail.com>
2026-09-12 12:09:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='madhyum-crm-v4-20260912-1';
+const CACHE='madhyum-crm-v4-20260912-2';
 const ASSETS=['./','./index.html','./css/app.css','./js/core.js','./js/api.js','./js/auth.js','./js/navigation.js','./js/agent.js','./js/admin.js','./js/app.js','./manifest.json','./madhyum-brand.png','./assets/dashboard-hero.jpg','./icon/icon-192.png','./icon/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
